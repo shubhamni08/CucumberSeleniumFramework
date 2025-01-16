b65df566ba7142fb2c197472f0a95a42081b2a4f
shubhamni08 <nikamshubham016@gmail.com>
2025-01-16 16:30:39 +0530

    Refactor code

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -7,16 +7,12 @@ import utility.DriverManager;
 public class BaseTest {
     protected WebDriver driver;
     public static String menulistXpath = "//*[@class='menu-list']/li/*[text()='%s']";
-
     public static String homePageCardXpath = "//*[@class='card-body']/*[text()='%s']";
 
     public BaseTest() {
         this.driver = DriverManager.getDriver(null); // Get the existing driver instance
     }
 
-    /**
-     * Handle unexpected alerts gracefully to avoid UnhandledAlertException.
-     */
     protected void dismissUnexpectedAlert() {
         try {
             driver.switchTo().alert().dismiss();
