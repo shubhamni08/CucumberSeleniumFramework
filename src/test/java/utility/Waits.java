a862931207053845eeb58cda00550af8dbb5258b
shubhamni08 <nikamshubham016@gmail.com>
2025-01-21 12:46:26 +0530

    Waits class updated

--- src/test/java/utility/Waits.java
+++ src/test/java/utility/Waits.java
@@ -2,18 +2,22 @@ package utility;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.function.Function;
 
 public class Waits {
 
-    public WebDriver driver;
+    private WebDriver driver;
 
-    public Waits(WebDriver driver) {
-        this.driver = driver;
+    private WebDriverWait wait;
+
+    public Waits() {
+        this.wait = new WebDriverWait(DriverManager.getDriver(null), Duration.ofSeconds(20));
     }
 
     public void waitForElement(WebElement element) {
@@ -42,6 +46,58 @@ public class Waits {
         } catch (NoSuchElementException | TimeoutException | StaleElementReferenceException e ){
             Assert.fail("Element not found on page: " + elementList);
         }
+    }
+
+    public void waitUntil(Runnable runnable) {
+        FluentWait<WebDriver> fluentWait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(20))
+                .pollingEvery(Duration.ofMillis(500))
+                .ignoring(NoSuchElementException.class, TimeoutException.class);
+
+        fluentWait.until(driver -> {
+            runnable.run();
+            return true;
+        });
+    }
+
+    public WebElement waitForElementVisibility(WebElement element){
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public WebElement waitForVisiblityOfElement(By locator){
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public WebElement waitForElementToBeClickable(By locator){
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    public void waitForElementToBeClickable(WebElement button) {
+        wait.until(ExpectedConditions.elementToBeClickable(button));
+    }
+
+    public WebElement waitForElementToBePresence(By locator){
+        return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    public Alert waitForAlertPresence() {
+        return wait.until(ExpectedConditions.alertIsPresent());
+    }
+
+    public void waitForCondition(Function<WebDriver, Boolean> condition) {
+        wait.until(condition);
+    }
+
+    public void waitForNumberOfWindow(int expectedwindow){
+        wait.until(ExpectedConditions.numberOfWindowsToBe(2));
+    }
+
+    public boolean waitForTextOfElementPresent(By locator, String text){
+       return wait.until(ExpectedConditions.textToBePresentInElementLocated(locator, text));
+    }
+
+    public boolean waitForAttributeToBe(WebElement element, String val1, String val2){
+        return wait.until(ExpectedConditions.attributeToBe(element, val1, val2));
     }
 
 }
